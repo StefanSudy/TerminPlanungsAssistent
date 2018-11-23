08edf2cb40411e4898cdfc0ee108aeda0a07bcee
Stefan Sudy <stefan.sudy@acp.at>
2018-11-23 15:12:30 +0100

    Removed Username from Usermodel

--- TPAWebApi/TPAWebApi/ModelDto/User.cs
+++ TPAWebApi/TPAWebApi/ModelDto/User.cs
@@ -11,8 +11,6 @@ namespace TPAWebApi.ModelDto
         [Required]
         public int Id { get; set; }
         [Required]
-        public string Username { get; set; }
-        [Required]
         public string EMail { get; set; }
         [Required]
         public string Password { get; set; }
