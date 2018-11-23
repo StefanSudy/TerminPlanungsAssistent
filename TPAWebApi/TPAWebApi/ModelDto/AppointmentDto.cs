3d4bd77220f3a816f536d86164d420ca1a0d60b3
Stefan Sudy <stefan.sudy@acp.at>
2018-11-23 14:17:18 +0100

    Created Models and UsersController

---
+++ TPAWebApi/TPAWebApi/ModelDto/AppointmentDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TPAWebApi.ModelDto
+{
+    public class AppointmentDto : IBaseModel
+    {
+        [Required]
+        public string EntryName { get; set; }
+        public string EntryText { get; set; }
+        public DateTime DateDue { get; set; }
+        [Required]
+        public DateTime DateCreated { get; set; }
+        public DateTime Duration { get; set; }
+        [Required]
+        public bool Status { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public int Id { get; set; }
+    }
+}
---
+++ TPAWebApi/TPAWebApi/ModelDto/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TPAWebApi.ModelDto
+{
+    public class User : IBaseModel
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string EMail { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public bool Active { get; set; }
+    }
+}
