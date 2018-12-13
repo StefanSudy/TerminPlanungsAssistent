faf157c19c3b822395c4fd79dee8520dd2627dd3
Andreas0711 <45166634+Andreas0711@users.noreply.github.com>
2018-12-13 12:01:05 +0100

    user validation start

--- TPAFrontEnd/src/pages/login/login.ts
+++ TPAFrontEnd/src/pages/login/login.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavController, LoadingController } from 'ionic-angular';
 import { RegisterPage } from '../register/register';
 import { HomePage } from '../home/home';
+import { APIService } from "../../providers/apiservice/apiservice";
+import { User } from '../../models/user';
 
 @Component({
   selector: 'page-login',
@@ -9,19 +11,28 @@ import { HomePage } from '../home/home';
 })
 export class Login {
 
-  email:string;
-  password:string;
+  email: string;
+  password: string;
+  user: User;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private restService: APIService) {
+    
   }
 
-  goHome(){
+  login(){
     //Überprüfung des Users
-
+    this.restService.PostValidateUser(this.email, this.password)
+    .subscribe(
+      (gotUser) => {
+        this.user = gotUser;
+      }
+    );
     if(true){ //User ist gültig
+      
     this.navCtrl.setRoot(HomePage);
-    this.navCtrl.popToRoot();
+    this.navCtrl.popToRoot({
+      user: this.user
+    });
     }else{
       alert("User ist nicht vorhanden.")
     }
