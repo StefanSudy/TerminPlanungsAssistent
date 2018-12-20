ceae4e5bf032b722419aafec1f6d850bd708d22b
Stefan Sudy <stefan.sudy@acp.at>
2018-12-20 11:10:17 +0100

    Revert "Merged devauthenticate into dev"
    This reverts commit f5db05198e35ff9b296c385c6ef94afd18c18c30.

--- TPAFrontEnd/src/pages/calendar/calendar.ts
+++ TPAFrontEnd/src/pages/calendar/calendar.ts
@@ -1,11 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
-import { ViewItemPage } from '../view-item/view-item';
-import moment from 'moment';
-//Api-Services
-import { APIService } from '../../providers/apiservice/apiservice';
-import { Appointment } from '../../models/appointment';
-//import { User } from '../../models/user';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
 /**
  * Generated class for the CalendarPage page.
  *
@@ -19,90 +14,12 @@ import { Appointment } from '../../models/appointment';
   templateUrl: 'calendar.html',
 })
 export class CalendarPage {
-  eventSource = [];
-  viewTitle: string;
-  selectedDay = new Date();
-  public currentItems: any = [];
- 
-  calendar = {
-    mode: 'month',
-    currentDate: new Date(),
-    locale: 'de-AT'
-  };
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private modalCtrl: ModalController, public restProvider: APIService) {
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  addEvent() {
-    let modal = this.modalCtrl.create('EventModalPage', {selectedDay: this.selectedDay});
-    modal.present();
-    modal.onDidDismiss(answer => {
-      if (answer) {
-        this.currentItems.push(answer);
-        let events = this.eventSource;
-        let endTime = moment(answer.dateDue).add(answer.duration, 'minutes').format();
-        this.eventSource = [];
-        events.push({
-          title: new String(answer.entryName),
-          startTime: new Date(answer.dateDue),
-          endTime: new Date(endTime),
-          allDay: false,
-          id: answer.id
-        });
-        setTimeout(() => {
-          this.eventSource = events;
-        });
-      }
-      
-    });
-  }
- 
-  onViewTitleChanged(title) {
-    this.viewTitle = title;
-  }
- 
-  onEventSelected(event) {
-    this.restProvider.GetAppointmentsForUser(+localStorage.getItem('user_id')).subscribe((currentItems : Appointment[]) => {
-      this.currentItems=currentItems;
-    });    
-    for(let item of this.currentItems){
-      if(item.id == event.id && event.id != null){ //Nur das gerade ausgewählte Element und wenn es in der Datenbank gefunden wurde.
-        this.navCtrl.push(ViewItemPage, {
-          currentItem: item
-        });
-      }
-    }
-  }
- 
-  onTimeSelected(ev) {
-    this.selectedDay = ev.selectedTime;
-    
-  }
-
-  ionViewWillEnter() {
-    this.restProvider.GetAppointmentsForUser(+localStorage.getItem('user_id'))
-    .subscribe((currentItems : Appointment[]) => {
-      this.currentItems=currentItems;
-
-    });    
-  }
-
-  ionViewDidEnter() {
-    let events = [];
-    let endTime;
-    for (let item of this.currentItems) {
-      if(item.dateDue != null && item.status == true){    //Nur wenn Datum vorhanden und Item noch nicht abgehandelt. 
-        endTime = moment(item.dateDue).add(item.duration, 'minutes').format();
-        events.push({
-          title: new String(item.entryName),
-          startTime: new Date(item.dateDue),
-          endTime: new Date(endTime),
-          allDay: false,
-          id: item.id
-        });
-      }
-    }
-    this.eventSource = events;
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad CalendarPage');
   }
 
 }
