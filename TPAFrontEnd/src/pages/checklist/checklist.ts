4c2ba34ad150615810472f44f74505eafc56f9d1
Stefan Sudy <stefan.sudy@acp.at>
2019-01-10 12:45:45 +0100

    design changes

--- TPAFrontEnd/src/pages/checklist/checklist.ts
+++ TPAFrontEnd/src/pages/checklist/checklist.ts
@@ -20,8 +20,6 @@ export class ChecklistPage {
   ionViewWillEnter() {
     var items = this.appointmentProvider.getAppointments();
     items.forEach(item => {
-      if (item.dateDue == new Date(-8640000000000000))
-        item.dateDue = null;
       this.currentItems.push({item: item, expanded: false});
     });
   }
